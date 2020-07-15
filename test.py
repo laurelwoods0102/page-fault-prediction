dc7641ac05372f12d8ee2d89870898fefaefc65c
Laurelwoods <laurelwoods0102@naver.com>
2020-07-15 18:11:05 +0900

    grid search

--- test.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-from tensorflow import keras
-
-physical_devices = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
